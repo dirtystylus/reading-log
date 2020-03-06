1e787f716350600f81c7bba41a8636d0e3ab6085
Mark Llobrera <mark@dirtystylus.com>
2020-03-06 10:20:37 -0500

    got time domain, working on tick marks

--- src/ReadingTimeline.js
+++ src/ReadingTimeline.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { OrdinalFrame } from "semiotic";
 import { scaleTime } from "d3-scale";
+import { timeFormat } from "d3-time-format";
 
 const timescale = scaleTime().domain([
   new Date(2000, 0, 1),
-  new Date(2000, 1, 28)
+  new Date(2000, 11, 31)
 ]);
 
 const colors = {
-  Democratic: 4,
-  Republican: 1,
-  "Democratic-Republican": 2,
-  Whig: 3
+  Fiction: 4,
+  Nonfiction: 1,
+  Poetry: 2
 };
 const theme = [
   "#ac58e5",
@@ -28,16 +28,22 @@ const theme = [
 const frameProps = {
   data: [
     {
-      name: "George Washington",
+      name: "The Regrets",
       start: new Date(2000, 0, 4),
       end: new Date(2000, 0, 5),
-      party: "None"
+      party: "Fiction"
     },
     {
-      name: "John Adams",
+      name: "Minor Feelings",
       start: new Date(2000, 0, 5),
       end: new Date(2000, 1, 2),
-      party: "Democratic"
+      party: "Poetry"
+    },
+    {
+      name: "Foundryside",
+      start: new Date(2000, 1, 5),
+      end: new Date(2000, 3, 2),
+      party: "Nonfiction"
     }
   ],
   size: [900, 800],
@@ -53,7 +59,7 @@ const frameProps = {
     fill: theme[colors[d.party] || 0],
     stroke: theme[(colors[d.party] || 0) + 5]
   }),
-  title: "U.S. Presidential Terms",
+  title: "Reading Log 2020",
   foregroundGraphics: Object.keys(colors)
     .concat(["Other"])
     .map((d, i) => (
